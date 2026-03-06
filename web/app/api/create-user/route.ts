f1721f177a7acb566add07db2103ab36b06f8935
Sn4xiii <mike.assmann@motherson.com>
2026-03-06 05:51:25 +0100

    -route

--- web/app/api/create-user/route.ts
+++ web/app/api/create-user/route.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+console.log("SERVICE KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY?.slice(0,10));
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
