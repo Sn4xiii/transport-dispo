bfe4fb2a93f42f40d5fe476af2bed857b6a20bd4
Sn4xiii <mike.assmann@motherson.com>
2026-03-06 06:45:34 +0000

    add

--- web/app/(app)/admin/components/admin/AdminLayout.tsx
+++ web/app/(app)/admin/components/admin/AdminLayout.tsx
@@ -6,15 +6,9 @@ export default function AdminLayout({children}:{children:React.ReactNode}){
 
 return(
 
-<div className="admin-layout">
-
-<AdminSidebar/>
-
 <div className="admin-content">
 
 {children}
-
-</div>
 
 </div>
 
